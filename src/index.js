64896fbb1d3ba59fb6cdaae774a92deefed1ac4b
Maja Dit <maja.ditrtova@outlook.com>
2023-12-10 13:35:06 +0000

    add api integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const generatePoem = (event) => {
-  event.preventDefault();
-
+const displayPoem = (response) => {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+};
+
+const generatePoem = (event) => {
+  event.preventDefault();
+
+  let userInput = document.getElementById("user-input");
+  let apiKey = "7b34357f3et00d865954fab83029o0ae";
+  let context =
+    "You are a romantic poem expert that likes to write short poems in French. Your mission is to generate 4-line poems. The poem must be displayed in 4 lines. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a French poem about ${userInput}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 };
 
 let poemFormElement = document.getElementById("poem-generator-form");
